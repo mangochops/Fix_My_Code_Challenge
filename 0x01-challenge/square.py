d59e30aa3cb56156e5316469ba05bedb72d87ee3
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-13 19:11:06 +0300

    Create square.py

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,22 @@
+class Square:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """Calculate the area of the square."""
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        """Calculate the perimeter of the square."""
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """String representation of the square."""
+        return f"Width: {self.width}, Height: {self.height}"
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
